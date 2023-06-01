757f3ed50a235afacd12ddce5f90649950654282
mitrahe <heydarimitra98@gmail.com>
2023-06-01 22:00:36 +0000

    Add ApplicationContext class to util package.

---
+++ src/main/java/org/example/util/ApplicationContext.java
@@ -0,0 +1,38 @@
+package org.example.util;
+
+import org.example.config.DatabaseConfig;
+import org.example.config.DatabaseConnector;
+import org.example.repository.AuthorRepository;
+import org.example.repository.AuthorRepositoryImpl;
+import org.example.repository.BookRepository;
+import org.example.repository.BookRepositoryImpl;
+import org.example.service.AuthorService;
+import org.example.service.AuthorServiceImpl;
+import org.example.service.BookServiceImpl;
+
+import java.sql.Connection;
+
+public class ApplicationContext {
+
+    private static Connection connection;
+    private static AuthorRepository authorRepository;
+    private static BookRepository bookRepository;
+    private static AuthorService authorService;
+    private static BookServiceImpl bookService;
+
+    static {
+        Connection connection = new DatabaseConnector().getConnection();
+        authorRepository = new AuthorRepositoryImpl(connection);
+        authorService = new AuthorServiceImpl(authorRepository);
+        bookRepository = new BookRepositoryImpl(connection);
+        bookService = new BookServiceImpl(bookRepository);
+    }
+
+    public static AuthorService getAuthorService() {
+        return authorService;
+    }
+
+    public static BookServiceImpl getBookService() {
+        return bookService;
+    }
+}
